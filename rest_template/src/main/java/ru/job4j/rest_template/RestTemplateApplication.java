03841ce4cdd7ded89ded26282dc8ba115508660a
zely20@mail.ru <Civik1984>
2021-09-13 21:36:59 +0300

    add module

---
+++ rest_template/src/main/java/ru/job4j/rest_template/RestTemplateApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.rest_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateApplication.class, args);
+    }
+
+}
---
+++ rest_template/src/test/java/ru/job4j/rest_template/RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.rest_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
